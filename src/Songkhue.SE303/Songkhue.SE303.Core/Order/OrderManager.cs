86cb7a76a4923cac7a3237626d2870d3b671e74c
Truc Nguyen <nguyencuongtruc@gmail.com>
2012-08-04 19:46:26 +0700

    Update OrderManager Class

---
+++ src/Songkhue.SE303/Songkhue.SE303.Core/Order/OrderManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Songkhue.SE303.Core.Order
+{
+    public class OrderManager
+    {
+        private Dev_Sk_SE303Entities _context;
+        private Repository<Sk_Order> _reporsitory;
+
+        public IQueryable<Sk_Order> GetOrders(DateTime date)
+        {
+            // remove time
+            return _reporsitory.Fetch().Where(x => DateTime.Parse(x.CreatedOn.ToShortDateString()) == date);
+        }
+    }
+}
