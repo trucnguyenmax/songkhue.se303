607d6e1eeaf82bb03e96a7d6c71574b44170a010
Truc Nguyen <nguyencuongtruc@gmail.com>
2012-08-04 18:38:06 +0700

    SRC
    Hello :)

---
+++ src/Songkhue.SE303/Songkhue.SE303.Core/Repository.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Objects;
+using System.Data;
+
+namespace Songkhue.SE303.Core
+{
+    public class Repository<T> where T : class
+    {
+        private ObjectContext _context;
+        private readonly IObjectSet<T> _objectSet;
+
+        public Repository(ObjectContext context)
+        {
+            _context = context;
+            _objectSet = _context.CreateObjectSet<T>();
+
+        }
+
+        public Repository(ObjectContext context, bool lazyLoading)
+        {
+            _context = context;
+            _objectSet = _context.CreateObjectSet<T>();
+            _context.ContextOptions.LazyLoadingEnabled = lazyLoading;
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public void SaveChanges(SaveOptions options)
+        {
+            _context.SaveChanges(options);
+        }
+
+        public IQueryable<T> Fetch()
+        {
+            return _objectSet;
+        }
+
+        public void Add(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+
+            _objectSet.AddObject(entity);
+
+        }
+
+        public void Edit(T entity)
+        {
+            _context.ObjectStateManager.ChangeObjectState(entity, EntityState.Unchanged);
+            _objectSet.Attach(entity);
+            _context.ObjectStateManager.ChangeObjectState(entity, EntityState.Modified);
+        }
+
+        public void Delete(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+
+            _objectSet.DeleteObject(entity);
+
+
+        }
+    }
+}
---
+++ src/Songkhue.SE303/Songkhue.SE303.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Songkhue.SE303.Web")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Songkhue.SE303.Web")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9d81919d-5954-4351-a381-2d9150a4a4d7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
