afd52867292663b2c76ad6e744aff7b954e67bc3
Truc Nguyen <nguyencuongtruc@gmail.com>
2012-08-05 10:29:43 +0700

    Item Manager Class

---
+++ src/Songkhue.SE303/Songkhue.SE303.Core/Item/ItemManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Songkhue.SE303.Core.Item
+{
+    public class ItemManager
+    {
+        private Dev_Sk_SE303Entities _context;
+        private Repository<Sk_Item> _reporsitory;
+
+        private ItemManager()
+        {
+            _context = new Dev_Sk_SE303Entities();
+            _reporsitory = new Repository<Sk_Item>(_context, true);
+        }
+
+        public void Add(Sk_Item item)
+        {
+            _reporsitory.Add(item);
+            _reporsitory.SaveChanges();
+        }
+
+        public void Update(Sk_Item item)
+        {
+            _reporsitory.Edit(item);
+            _reporsitory.SaveChanges();
+        }
+
+        public void Delete(Sk_Item item)
+        {
+            _reporsitory.Delete(item);
+            _reporsitory.SaveChanges();
+        }
+
+        public IQueryable<Sk_Item> GetItems()
+        {
+            return _reporsitory.Fetch();
+        }
+    }
+}
--- src/Songkhue.SE303/Songkhue.SE303.Core/Order/OrderManager.cs
+++ src/Songkhue.SE303/Songkhue.SE303.Core/Order/OrderManager.cs
@@ -10,6 +10,12 @@ namespace Songkhue.SE303.Core.Order
         private Dev_Sk_SE303Entities _context;
         private Repository<Sk_Order> _reporsitory;
 
+        private OrderManager()
+        {
+            _context = new Dev_Sk_SE303Entities();
+            _reporsitory = new Repository<Sk_Order>(_context, true);
+        }
+
         public IQueryable<Sk_Order> GetOrders(DateTime date)
         {
             // remove time
