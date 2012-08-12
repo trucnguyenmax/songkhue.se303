0c996cfb06947b2cbee37130cc71a76323c86b7d
Truc Nguyen <nguyencuongtruc@gmail.com>
2012-08-12 18:58:21 +0700

    Employee

---
+++ src/Songkhue.SE303/Songkhue.SE303.Web/Areas/Admin/Controllers/EmployeeController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Songkhue.SE303.Core;
+using Songkhue.SE303.Web.Models;
+
+namespace Songkhue.SE303.Web.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+        Dev_Sk_SE303Entities db = new Dev_Sk_SE303Entities();
+        public ActionResult Index()
+        {
+            return View(EmployeeModels.employeeList());
+        }
+
+        //
+        // GET: /Employee/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View(EmployeeModels.detail(id));
+        }
+
+        //
+        // GET: /Employee/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Employee/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                EmployeeModels.create(collection);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+        //
+        // GET: /Employee/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            return View(EmployeeModels.detail(id));
+        }
+
+        //
+        // POST: /Employee/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                EmployeeModels.edit(collection, id);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Employee/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            return View(EmployeeModels.detail(id));
+        }
+
+        //
+        // POST: /Employee/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                EmployeeModels.delete(id);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ src/Songkhue.SE303/Songkhue.SE303.Web/Areas/Admin/Models/EmployeeModels.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Songkhue.SE303.Core;
+using System.Web.Mvc;
+namespace Songkhue.SE303.Web.Models
+{
+    public class EmployeeModels
+    {
+        public static void create(FormCollection collection)
+        {
+            using(var db = new Dev_Sk_SE303Entities())
+            {
+                Sk_User u = new Sk_User();
+                u.Username = collection["Username"].ToString();
+                u.CreatedOn = DateTime.Today;
+                u.Email = collection["Email"].ToString();
+                db.Sk_User.AddObject(u);
+                db.SaveChanges();
+            }
+        }
+        public static void edit(FormCollection collection,int id)
+        {
+            using (var db = new Dev_Sk_SE303Entities())
+            {
+                Sk_User u = db.Sk_User.FirstOrDefault(c => c.Id == id);
+                u.Username = collection["Username"].ToString();
+                u.CreatedOn = DateTime.Today;
+                u.Email = collection["Email"].ToString();
+                db.SaveChanges();
+            }
+        }
+        public static Sk_User detail(int id)
+        {
+            using (var db = new Dev_Sk_SE303Entities())
+            {
+                return db.Sk_User.FirstOrDefault(c => c.Id == id);
+            }
+            return null;
+        }
+        public static void delete(int id)
+        {
+            using (var db = new Dev_Sk_SE303Entities())
+            {
+                db.DeleteObject(db.Sk_User.FirstOrDefault(c => c.Id == id));
+                db.SaveChanges();
+            }
+        }
+        public static List<Sk_User> employeeList()
+        {
+            using (var db = new Dev_Sk_SE303Entities())
+            {
+                return db.Sk_User.ToList();
+            }
+            return null;
+        }
+    }
+}
